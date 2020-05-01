f91f1d998d6b50646c0b4130cd0da9fea632e4f8
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-05-01 01:02:21 +0100

    backlog

--- controllers/psycho.js
+++ controllers/psycho.js
@@ -2,9 +2,43 @@ const express = require('express');
 const router = express.Router();
 const Review = require('../models/reviews.js');
 
+//SHOW
+router.get('/', (req, res) => {
+  Review.find({}, (err, allReviews) => {
+    res.render('psycho/show.ejs', {reviews:allReviews})
+  });
+});
 
+//NEW
+router.get('/new', (req, res) => {
+  res.render('psycho/new.ejs')
+});
 
+//CREATE
+router.post('/', (req, res) => {
+  Review.create(req.body, (error, createdReview) => {
+    res.redirect('psycho/')
+  });
+});
 
+//EDIT
+router.get('/:id/edit', (req, res) => {
+  Review.findById(req.params.id, (err, foundReview) => {
+    res.render('psycho/edit.ejs', {reviews:foundReview})
+  });
+});
 
+//PUT
+router.put('/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedReview) => {
+    res.redirect('/psycho')
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Review.findByIdAndDelete(req.params.id, (err, data) => {
+    res.redirect('/psycho')
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'));
 
 // Connect to Mongo
 mongoose.connect(process.env.DATABASE_URL, dbupdateobject);
@@ -27,21 +28,10 @@ db.on('open', () => {
 
 
 //CONTROLLERS
-//dialM
-const dialMController = require('./controllers/dialM.js');
-app.use('/dialM', dialMController);
-//northBy
-const northByController = require('./controllers/northBy.js');
-app.use('/northBy', northByController);
 //osyco
 const psychoController = require('./controllers/psycho.js');
 app.use('/psycho', psychoController);
-//strangers
-const strangersController = require('./controllers/strangers.js');
-app.use('/strangers', strangersController);
-//vertigo
-const vertigoController = require('./controllers/vertigo.js');
-app.use('/vertigo', vertigoController);
+
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
